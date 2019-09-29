0243b0b46ca3b45f9649fcf3024324fb2ab3ac30
Salwa Badreddine <gb0684@wayne.edu>
2019-09-29 09:04:38 -0400

    URL for connection between app and door added

--- app/src/main/java/com/grizzhacks/pizzatruck/StopActivity.java
+++ app/src/main/java/com/grizzhacks/pizzatruck/StopActivity.java
@@ -3,19 +3,11 @@ package com.grizzhacks.pizzatruck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.ifttt.Applet;
-import com.ifttt.ErrorResponse;
-import com.ifttt.IftttApiClient;
-import com.ifttt.api.AppletsApi;
-import com.ifttt.api.PendingResult;
-
-import java.util.List;
 
 public class StopActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -33,21 +25,8 @@ public class StopActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void onButtonStop() {
-        AppletsApi appletsApi = IftttApiClient.getInstance().appletsApi();
-        appletsApi.listApplets("GWVfs5b4GzoddG0cudz2WutzYjnFBGj5vwx5kBh3OaZIfo6rZWSEfuTGPvHMlYnE", AppletsApi.Platform.android, AppletsApi.Order.enabled_count_asc)
-                .execute(new PendingResult.ResultCallback<List<Applet>>() {
-                    @Override
-                    public void onSuccess(List<Applet> applets) {
-                        finish();
-                        startActivity(new Intent(StopActivity.this, PizzaReadyActivity.class));
-                    }
-
-                    @Override
-                    public void onFailure(ErrorResponse errorResponse) {
-                        Toast.makeText(StopActivity.this, "FAILED, CONTACT STORE FOR DETAILS",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("172.20.10.4/gpio/1"));
+        startActivity(browserIntent);
     }
 
     @Override
