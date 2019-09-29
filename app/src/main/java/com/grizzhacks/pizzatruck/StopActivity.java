f3fcde9f5110dbb11ad32df3d3f73628524b24f9
Salwa Badreddine <gb0684@wayne.edu>
2019-09-29 07:25:36 -0400

    IFTTT setup

--- app/src/main/java/com/grizzhacks/pizzatruck/StopActivity.java
+++ app/src/main/java/com/grizzhacks/pizzatruck/StopActivity.java
@@ -2,13 +2,59 @@ package com.grizzhacks.pizzatruck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class StopActivity extends AppCompatActivity {
+import com.ifttt.Applet;
+import com.ifttt.ErrorResponse;
+import com.ifttt.IftttApiClient;
+import com.ifttt.api.AppletsApi;
+import com.ifttt.api.PendingResult;
+
+import java.util.List;
+
+public class StopActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop);
+
+        buttonStop = (Button) findViewById(R.id.buttonStop);
+
+        //attaching listener to button
+        buttonStop.setOnClickListener(this);
     }
+
+    private void onButtonStop() {
+        AppletsApi appletsApi = IftttApiClient.getInstance().appletsApi();
+        appletsApi.listApplets("GWVfs5b4GzoddG0cudz2WutzYjnFBGj5vwx5kBh3OaZIfo6rZWSEfuTGPvHMlYnE", AppletsApi.Platform.android, AppletsApi.Order.enabled_count_asc)
+                .execute(new PendingResult.ResultCallback<List<Applet>>() {
+                    @Override
+                    public void onSuccess(List<Applet> applets) {
+                        finish();
+                        startActivity(new Intent(StopActivity.this, PizzaReadyActivity.class));
+                    }
+
+                    @Override
+                    public void onFailure(ErrorResponse errorResponse) {
+                        Toast.makeText(StopActivity.this, "FAILED, CONTACT STORE FOR DETAILS",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonStop) {
+            onButtonStop();
+        }
+    }
+
 }
